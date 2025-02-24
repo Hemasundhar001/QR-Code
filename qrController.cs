626655ab91e9802d1326569a8337f5fa7cd9f704
Hemasundhar <116507961+Hemasundhar001@users.noreply.github.com>
2025-02-24 22:12:15 +0530

    controller code
    its a controller code

---
+++ qrController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using qrcode.Models;
+using QRCoder;
+using static QRCoder.PayloadGenerator;
+
+namespace qrcode.Controllers
+{
+    public class qrController : Controller
+    {
+       
+        // GET: qr
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Class122());
+        }
+        [HttpPost]
+        public ActionResult Index(Class122 class1)
+        {
+            try
+            {
+
+
+                Payload payload = new Url(class1.Qrcodetext);
+                QRCodeGenerator codeGenerator = new QRCodeGenerator();
+                QRCodeData qRCodeData = codeGenerator.CreateQrCode(payload);
+                QRCoder.PngByteQRCode pngq = new PngByteQRCode(qRCodeData);
+                var Qrbyte = pngq.GetGraphic(20);
+                string base64Url = Convert.ToBase64String(Qrbyte);
+                class1.Qrimageurl = "data:image/png;base64," + base64Url;
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return View("Index", class1);
+        }
+    }
+}
