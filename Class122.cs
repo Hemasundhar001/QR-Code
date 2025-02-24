dfd4641b940dd2cc13213a448c8b0b287045d5fa
Hemasundhar <116507961+Hemasundhar001@users.noreply.github.com>
2025-02-24 22:11:39 +0530

    model code
    Its a model code

---
+++ Class122.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace qrcode.Models
+{
+    public class Class122
+    {
+        public string Qrcodetext { get; set; }
+        public string Qrimageurl { get; set; }
+    }
+}
